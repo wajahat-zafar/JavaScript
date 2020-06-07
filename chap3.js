c7f42c583662329e54b3af794c26102d03b1af9f
Muhammad Wajahat <45312669+wajahat-zafar@users.noreply.github.com>
2020-06-07 16:30:46 +0500

    javascript
    Practice Assignment

---
+++ chap3.js
@@ -0,0 +1,17 @@
+//Chap3
+
+var age = "20"
+alert("I am" + " " + age + " " + "year old")
+
+var visits = "15"
+alert("You have visited" + " " + visits + " " + "times this page")
+
+birthYear = "1999"
+document.write("My birth year is" + " " + birthYear + "<br>" + "Datatype of my declared variable is number"+ "<br>")
+
+
+var visitor = "Jhon Doe"
+var product = "Shoes"
+var Quantity = "3 pieces"
+document.write(visitor + " " +  "want to order" + " " + Quantity + " " + product + " " + "from xyz shopping store")
+
